3bd665c7e37f11500b5f9f03267024baf3960b1c
Amit Buchnik <amit.buchnik@motorolasolutions.com>
2020-08-06 12:27:28 +0300

    heroku support

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/whether'));
+app.use(express.static('./dist/wheather'));
 
 app.get('/*', function (req, res) {
   res.sendFile('index.html', {
-    root: 'dist/whether'
+    root: 'dist/wheather'
   });
 });
 

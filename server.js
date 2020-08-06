27b3692b921993cbc3cff844741e3eb891246525
Amit Buchnik <amit.buchnik@motorolasolutions.com>
2020-08-06 13:42:20 +0300

    fdfds

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
+//Install express server    
 const express = require('express');
+
+const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/wheather'));
+// Serve only the static files form the dist directory    
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function (req, res) {
-  res.sendFile('index.html', {
-    root: 'dist/wheather'
-  });
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
+// Start the app by listening on the default Heroku port    
 app.listen(process.env.PORT || 8080);
-
-console.log(`Running on port ${process.env.PORT || 8080}`)

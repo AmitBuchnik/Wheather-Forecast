880a31461d245dadf0fbb5947ccf9f9195d97a91
Amit Buchnik <amit.buchnik@motorolasolutions.com>
2020-08-09 17:32:06 +0300

    new apikey

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const path = require('path');
 
 const app = express();
 
+app.options('*', cors());
+
 // Serve only the static files form the dist directory    
 app.use(express.static(__dirname + '/dist/wheather'));
 
